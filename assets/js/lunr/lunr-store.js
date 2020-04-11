e18640a7ef4eda93307b8e0d1d7e6b9f380c83be
tars-c <securemem@icloud.com>
2020-04-11 19:39:53 +0900

    이니셜라이저 포스팅 및 블로그 전체 레이아웃 개편

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "[C++] 멤버이니셜라이저(Member-Initializer)",
+        "excerpt":"서론 멤버변수의 선언 후 기본값이 Visual Studio에서는 디버그 환경으로 빌드할 시 0이 아닌 0xCDCDCD라는 이상한 값이 들어가 있습니다. 기본값이 잘못되어 런타임에서 프로그램이 실행되다가 갑자기 멈춘다거나 결과물이 다르게 출력되는 현상이 나타납니다. 값을 초기화 한다는 것은 프로그래밍에서 매우 중요한 역할을 합니다. 클래스 멤버변수를 초기화하려면 생성자를 이용하거나 멤버이니셜라이저를 이용합니다. 레퍼런스 타입을 가진 멤버변수...","categories": ["Cpp"],
+        "tags": ["타스","프로그래밍","Cpp","C++","멤버이니셜라이저","Member-Initializer"],
+        "url": "https://tars-c.github.io/cpp/C++%EB%A9%A4%EB%B2%84%EC%9D%B4%EB%8B%88%EC%85%9C%EB%9D%BC%EC%9D%B4%EC%A0%80/",
+        "teaser":null}]
