f81012102ceeb08d8fe5f4d9b4aa6a2880f671fb
tars-c <securemem@icloud.com>
2020-04-12 17:00:31 +0900

    모달 및 모달리스 포스팅

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,10 @@ var store = [{
         "title": "[C++] 멤버이니셜라이저(Member-Initializer)",
         "excerpt":"서론 멤버변수의 선언 후 기본값이 Visual Studio에서는 디버그 환경으로 빌드할 시 0이 아닌 0xCDCDCD라는 이상한 값이 들어가 있습니다. 기본값이 잘못되어 런타임에서 프로그램이 실행되다가 갑자기 멈춘다거나 결과물이 다르게 출력되는 현상이 나타납니다. 값을 초기화 한다는 것은 프로그래밍에서 매우 중요한 역할을 합니다. 클래스 멤버변수를 초기화하려면 생성자를 이용하거나 멤버이니셜라이저를 이용합니다. 레퍼런스 타입을 가진 멤버변수...","categories": ["Cpp"],
         "tags": ["타스","프로그래밍","Cpp","C++","멤버이니셜라이저","Member-Initializer"],
-        "url": "https://tars-c.github.io/cpp/C++%EB%A9%A4%EB%B2%84%EC%9D%B4%EB%8B%88%EC%85%9C%EB%9D%BC%EC%9D%B4%EC%A0%80/",
+        "url": "https://tars-c.github.io/cpp/C++_%EB%A9%A4%EB%B2%84%EC%9D%B4%EB%8B%88%EC%85%9C%EB%9D%BC%EC%9D%B4%EC%A0%80/",
+        "teaser":null},{
+        "title": "[MFC] 모달 및 모달리스",
+        "excerpt":"서론 윈도우즈에서 사용되는 Dialog는 2가지 종류로 구분됩니다. Modal Dialog 와 Modeless Dialog가 있습니다. 둘은 서로 비슷하면서 다른 특징을 가지고 있습니다. 윈도우즈에서 사용되는 Dialog는 부모 창이 종료되면 자동으로 종료되는 특징을 가지고 있습니다. CDialog 타입의 객체를 생성했을 경우 메시지 및 이벤트 처리가 상당히 복잡할 수 있습니다. CDialog, CDialogEx 클래스를 상속받아 자식 클래스를...","categories": ["MFC"],
+        "tags": ["타스","프로그래밍","Cpp","C++","MFC","모달","모달리스","다이얼로그","Modal","Modeless","Dialog"],
+        "url": "https://tars-c.github.io/mfc/MFC_%EB%AA%A8%EB%8B%AC%EA%B3%BC-%EB%AA%A8%EB%8B%AC%EB%A6%AC%EC%8A%A4/",
         "teaser":null}]
